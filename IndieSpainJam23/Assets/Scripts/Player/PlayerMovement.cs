3d277bf475300cda20fca8483b6609454aaf1d63
Laura Almón Manzano <almonmanzano@gmail.com>
2023-11-01 17:42:01 +0100

    Refactor PlayerMovement.cs

--- IndieSpainJam23/Assets/Scripts/Player/PlayerMovement.cs
+++ IndieSpainJam23/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Animator m_animator;
     [SerializeField] private float m_moveSpeed = 5f;
-    [SerializeField] private bool m_canDash = false;
+    [SerializeField] private bool m_dashIsEnabled = false;
     [SerializeField] private float m_dashSpeed = 25f;
     [SerializeField] private float m_dashDuration = 0.3f;
     [SerializeField] private float m_dashCd = 1f;
@@ -24,8 +24,6 @@ public class PlayerMovement : MonoBehaviour
     private float m_dashTime = 0f;
     private float m_timeSinceDash;
 
-    private bool m_isFlipped = false;
-
     private float m_timeSinceStep = 0f;
 
     private AudioSource m_stepsAudioSource;
@@ -34,86 +32,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
+        InitializeVariables();
+
+        HotelManager.Instance.SetPlayer(gameObject);
+    }
+
+    private void InitializeVariables()
+    {
         m_rb = GetComponent<Rigidbody2D>();
         m_trailRenderer = GetComponent<TrailRenderer>();
         m_timeSinceDash = m_dashCd;
         m_initialPosition = transform.position;
         m_timeSinceStep = m_timeBetweenStepsSFX;
-
-        HotelManager.Instance.SetPlayer(gameObject);
     }
 
     private void Update()
     {
-        if (!GameManagement.Instance.IsPlayable()) return;
+        if (!GameManagement.Instance.IsPlayable())
+            return;
 
         // Audio
-        m_timeSinceStep += Time.deltaTime;
-        if (m_timeSinceStep > m_timeBetweenStepsSFX && m_rb.velocity != Vector2.zero)
-        {
-            m_timeSinceStep = 0f;
-            int rand = Random.Range(0, m_stepsAudioClips.Length);
-            m_stepsAudioSource.clip = m_stepsAudioClips[rand];
-            m_stepsAudioSource.Play();
-        }
+        UpdateAudio();
 
         // Movement FX
-        m_timeSinceMovementFX += Time.deltaTime;
-        if (m_rb.velocity != Vector2.zero)
-        {
-            if (m_timeSinceMovementFX > m_timeBetweenMovementFX)
-            {
-                m_timeSinceMovementFX = 0f;
-                m_movementFX.Play();
-            }
-        }
+        UpdateMovementFX();
 
-        m_movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-
-        // Flip
-        if (!Mathf.Approximately(m_movement.x, 0f))
-        {
-            transform.localScale = new Vector3(m_movement.x > 0f ? 1f : -1f, 1f, 1f);
-            m_isFlipped = !(m_movement.x > 0f);
-        }
+        // Movement
+        UpdateMovementDirection();
 
         // Animation
         m_animator.SetBool("IsRunning", m_movement != Vector2.zero);
 
         // Dash
-        if (m_canDash)
-        {
-            if (!m_isDashing)
-            {
-                if (Input.GetKeyDown(KeyCode.Space) && m_movement != Vector2.zero && m_timeSinceDash >= m_dashCd)
-                {
-                    m_isDashing = true;
-                    m_dashTime = 0f;
-                    m_timeSinceDash = 0f;
-                    if (m_dashFX) Instantiate(m_dashFX, transform.position, transform.rotation);
-                    if (m_trailRenderer)
-                    {
-                        m_trailRenderer.emitting = true;
-                    }
-                }
-                else
-                {
-                    m_timeSinceDash += Time.deltaTime;
-                }
-            }
-            else
-            {
-                m_dashTime += Time.deltaTime;
-                if (m_dashTime >= m_dashDuration)
-                {
-                    m_isDashing = false;
-                    if (m_trailRenderer)
-                    {
-                        m_trailRenderer.emitting = false;
-                    }
-                }
-            }
-        }
+        UpdateDash();
     }
 
     private void FixedUpdate()
@@ -125,8 +76,99 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Movement
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
         float speed = m_isDashing ? m_dashSpeed : m_moveSpeed;
         m_rb.velocity = m_movement * speed;
+    }
+
+    private void UpdateMovementDirection()
+    {
+        m_movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // Flip
+        if (!Mathf.Approximately(m_movement.x, 0f))
+            transform.localScale = new Vector3(m_movement.x > 0f ? 1f : -1f, 1f, 1f);
+    }
+
+    private void UpdateDash()
+    {
+        if (!m_dashIsEnabled)
+            return;
+
+        if (!m_isDashing)
+        {
+            m_timeSinceDash += Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.Space) && CanDash())
+            {
+                StartDash();
+            }
+        }
+        else
+        {
+            m_dashTime += Time.deltaTime;
+
+            if (m_dashTime >= m_dashDuration)
+            {
+                EndDash();
+            }
+        }
+    }
+
+    private bool CanDash()
+    {
+        return m_movement != Vector2.zero && m_timeSinceDash >= m_dashCd;
+    }
+
+    private void StartDash()
+    {
+        m_isDashing = true;
+        m_dashTime = 0f;
+        m_timeSinceDash = 0f;
+
+        if (m_dashFX)
+            Instantiate(m_dashFX, transform.position, transform.rotation);
+
+        if (m_trailRenderer)
+            m_trailRenderer.emitting = true;
+    }
+
+    private void EndDash()
+    {
+        m_isDashing = false;
+        if (m_trailRenderer)
+            m_trailRenderer.emitting = false;
+    }
+
+    private void UpdateMovementFX()
+    {
+        m_timeSinceMovementFX += Time.deltaTime;
+        if (m_rb.velocity != Vector2.zero && m_timeSinceMovementFX > m_timeBetweenMovementFX)
+        {
+            m_timeSinceMovementFX = 0f;
+            m_movementFX.Play();
+        }
+    }
+
+    private void UpdateAudio()
+    {
+        m_timeSinceStep += Time.deltaTime;
+        if (m_timeSinceStep > m_timeBetweenStepsSFX && m_rb.velocity != Vector2.zero)
+        {
+            m_timeSinceStep = 0f;
+            PlayRandomAudio(m_stepsAudioClips);
+        }
+    }
+
+    private void PlayRandomAudio(AudioClip[] clips)
+    {
+        int rand = Random.Range(0, clips.Length);
+        m_stepsAudioSource.clip = clips[rand];
+        m_stepsAudioSource.Play();
     }
 
     public void SetStepsAudioSource(AudioSource audioSource)
@@ -147,12 +189,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void AddDash()
     {
-        m_canDash = true;
+        m_dashIsEnabled = true;
     }
 
     public bool IsFlipped()
     {
-        return m_isFlipped;
+        return m_movement.x <= 0f;
     }
 
     public void StopRunningAnimation()
